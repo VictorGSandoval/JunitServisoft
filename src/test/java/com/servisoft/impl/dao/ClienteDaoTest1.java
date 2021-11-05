00e382364d0531bb2b407975fba53e45af99f41b
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2021-11-05 12:16:51 -0500

    JUnit test mejorate

--- src/test/java/com/servisoft/impl/dao/ClienteDaoTest1.java
+++ src/test/java/com/servisoft/impl/dao/ClienteDaoTest1.java
@@ -7,9 +7,11 @@ package com.servisoft.impl.dao;
 
 import com.servisoft.impl.Conexion;
 import com.servisoft.model.ClienteModelo;
+import com.servisoft.model.ProductoModelo;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import static org.castor.core.util.Messages.message;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -83,19 +85,16 @@ public class ClienteDaoTest1 {
 //        ClienteModelo clientFromDb = dao.getClientByName("Joseph");
 //        assertEquals("Joseph", clientFromDb.getNombre());
 //    }
-
 //    @Test
 //    public void testEliminar() throws Exception {
-//        System.out.println("eliminar");
+//        System.out.println("Actualizar Estado");
 //        ClienteModelo cliente = new ClienteModelo();
 //        cliente.setDni("60323413");
-//        cliente.setCodigo(102);
+//        cliente.setCodigo(101);
 //        dao.eliminar(cliente);
-//        ClienteModelo clientFromDb = dao.getClientByName("Joseph");
+//        ClienteModelo clientFromDb = dao.getClientByName("Victor");
 ////        assertNull(clientFromDb.getNombre());
 //    }
-
-    
 //    @Test
 //    public void testModificar() throws Exception {
 //        System.out.println("modificar");
@@ -114,23 +113,56 @@ public class ClienteDaoTest1 {
 //        ClienteModelo clientFromDb = dao.getClientByName("Victor");
 //        assertEquals("Victor", clientFromDb.getNombre());
 //    }
+//    @Test
+//    public void testPrecioTotal() throws Exception {
+//        System.out.println("Calculo de precio Total");
+//        ProductoModelo prod = new ProductoModelo();
+//        prod.setPrecio(8);
+//        prod.setStock(3);
+//        long total = prod.getPrecio() * prod.getStock();
+//        assertEquals(24, total); // SET 30
+//    }
+
+//    @Test
+//    public void testActStock() throws Exception {
+//        System.out.println("Actualizar Stock");
+//        int comprar = 5; //5 ARTICULOS
+//        ProductoModelo prod = new ProductoModelo();
+//        prod.setStock(30);
+//        int descontar = prod.getStock() - comprar;
+////        prod.setPrecio(8);
+////        long total = prod.getPrecio() * prod.getStock();
+//        assertEquals(30, comprar+descontar);
+//    }
+    
+    
+//    @Test
+//    public void testValidNull() throws Exception {
+//        System.out.println("No permision Null");
+//        Integer comprar = 5; //SET NULL
+//        ProductoModelo prod = new ProductoModelo();
+//        prod.setStock(30);
+//        String message = "No puede comprar 0 productos";
+////        assertNull(message, comprar);
+//        assertNotNull(message, comprar);
+//    }
 
     /**
      * Test of registrar method, of class ClienteDao.
      */
-    @Test
-    public void testBuscarDNI2() {
-        ClienteModelo cli = new ClienteModelo();
-        ClienteDao instance = new ClienteDao();
-        final String dni = "40728524";
-        cli.setDni(dni);
-        try {
-            instance.buscarDNI(cli);
-            System.out.println(cli.getNombre());
-            assertEquals("Shirley Jackeline", cli.getNombre());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    @Test
+//    public void testBuscarDNI2() {
+//        ClienteModelo cli = new ClienteModelo();
+//        ClienteDao instance = new ClienteDao();
+//        final String dni = "40728524";
+//        cli.setDni(dni);
+//        try {
+//            instance.buscarDNI(cli);
+//            System.out.println(cli.getNombre());
+//            assertEquals("Shirley Jackeline", cli.getNombre());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
